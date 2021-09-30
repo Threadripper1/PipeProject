6cafbb3c239141ed42905af7437fe69c0c677726
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-09-30 18:58:23 +0300

    added terraform

---
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -0,0 +1,12 @@
+//DevOps practice
+
+provider "aws" {
+    access_key = "AKIAU7YO3OD34EVNO3V6"
+    secret_key = "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"
+    region = "us-east-2"
+}
+
+resource "aws_instance" "Jenkins_Serv" {
+    ami = "ami-00399ec92321828f5"
+    instance_type = "t2.micro"
+}
