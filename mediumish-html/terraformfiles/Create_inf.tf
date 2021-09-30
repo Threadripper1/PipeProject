b7192182be3a1bc9370d360f28633e6ae1e2c58d
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-09-30 21:46:38 +0300

    Ended terraform configuration

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -1,12 +1,12 @@
 //DevOps practice
 
 provider "aws" {
-    access_key = "AKIAU7YO3OD34EVNO3V6"
-    secret_key = "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"
+    access_key = "AKIAU7YO3OD35DBWL3JJ"
+    secret_key = "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"
     region = "us-east-2"
 }
 
-resource "aws_instance" "Jenkins_Serv" {
+resource "aws_instance" "MainServer" {
     ami = "ami-00399ec92321828f5"
     instance_type = "t2.micro"
 }
