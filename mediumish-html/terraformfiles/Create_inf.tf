1aceb33be8cc885d98e0b5f76df03dab8ea14f41
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-01 03:32:09 +0300

    Updated terraform file

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -6,7 +6,12 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "MainServer" {
-    ami = "ami-00399ec92321828f5"
+resource "aws_instance" "Main" {
+    ami = "ami-00dfe2c7ce89a450b"
     instance_type = "t2.micro"
+
+    tags = {
+      Name = "Main Server"
+      Owner = "Dmytro Kolomiiets"
+    }
 }
