610f7d8932b58d0be76ff0260f4e3f0d554771ff
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-04 22:59:35 +0300

    New commit

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -12,8 +12,6 @@ terraform {
 provider "aws" {}
 
 resource "aws_instance" "Main"{
-  vpc = true
-  network_interface = ec2-18-189-99-123.us-east-2.compute.amazonaws.com
   ami = "ami-00dfe2c7ce89a450b"
   instance_type = "t2.micro"
 
