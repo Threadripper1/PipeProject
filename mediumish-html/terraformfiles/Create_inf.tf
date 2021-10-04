857dd34fba7b09c76c48e8926b58ceb584c6b747
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-05 01:06:40 +0300

    Update terraform file

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "Main"{
   ami = "ami-00dfe2c7ce89a450b"
   instance_type = "t2.micro"
 
-  user_data = file("/var/lib/jenkins/.ssh/id_rsa.pub")
+  user_data = file("/var/lib/jenkins/.ssh/id_rsa")
 
   tags = {
     Name = "Web Server"
