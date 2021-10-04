ba50e66f517b43cdaf65b5bc56208db354d84333
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-04 23:40:17 +0300

    New commit

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -15,6 +15,9 @@ resource "aws_instance" "Main"{
   ami = "ami-00dfe2c7ce89a450b"
   instance_type = "t2.micro"
 
+  subnet_id = subnet-d9d851b2
+  private_ip = "172.31.5.250"
+
   tags = {
     Name = "Web Server"
     Owner = "Dmytro Kolomiiets"
