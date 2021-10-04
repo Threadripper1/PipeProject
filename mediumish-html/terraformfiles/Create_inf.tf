84547e11e9ce5859a3b47eaf1ff46062fc7054c1
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-04 23:56:00 +0300

    Updated terraform file

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -9,13 +9,30 @@ terraform {
   }
 }
 
+resource "aws_vpc" "default" {
+  cidr_block           = "172.31.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default.vpc-d4f395bf
+}
+
+resource "aws_subnet" "tf_test_subnet" {
+  vpc_id                  = aws_vpc.default.vpc-d4f395bf
+  cidr_block              = "172.31.0.0/20"
+  map_public_ip_on_launch = true
+
+  depends_on = [aws_internet_gateway.igw-855152ed]
+}
+
 provider "aws" {}
 
 resource "aws_instance" "Main"{
   ami = "ami-00dfe2c7ce89a450b"
   instance_type = "t2.micro"
 
-  subnet_id = subnet-d9d851b2
+  subnet_id = aws_subnet.tf_test_subnet.subnet-d9d851b2
   private_ip = "172.31.5.250"
 
   tags = {
