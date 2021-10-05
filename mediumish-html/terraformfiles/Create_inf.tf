f913d71aa2ab83e57c981230a00e946546a556d5
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-05 19:58:10 +0300

    new commit

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -28,9 +28,8 @@ resource "aws_instance" "WebServer"{
 
   user_data = file("/var/lib/jenkins/ssh_connection.sh")
 
-  provisioner "local-exec" { 
-    command = "PRIVATE_SSH_KEY='${tls_private_key.key.private_key_pem}'"
-    //command = "echo '${tls_private_key.key.private_key_pem}' >> ${aws_key_pair.generated_key.key_name}.pem"
+  provisioner "remote-exec" { 
+    inline = "cd ~/.ssh && echo '${tls_private_key.key.private_key_pem}' >> ${aws_key_pair.generated_key.key_name}.pem"
   }
 
   tags = {
