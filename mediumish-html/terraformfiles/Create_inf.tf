79fde0a4219204233f1a4b65ea2d972cac4949a1
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-05 19:02:50 +0300

    New commit

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "WebServer"{
 
   provisioner "local-exec" { 
     working_dir = "/home/ubuntu"
-    command = "touch WebServer.pem && echo '${tls_private_key.key.private_key_pem}' >> ./${aws_key_pair.generated_key.key_name}.pem "
+    command = "echo '${tls_private_key.key.private_key_pem}' > ${aws_key_pair.generated_key.key_name}.pem "
   }
 
   tags = {
