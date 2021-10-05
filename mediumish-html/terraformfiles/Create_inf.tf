d630a2ab6e3501ff03db8498580ae5672f8c394f
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-05 16:37:16 +0300

    New commit

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -9,11 +9,24 @@ terraform {
   }
 }
 
+variable "key_name" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.example.public_key_openssh
+}
+
 provider "aws" {}
 
 resource "aws_instance" "Main"{
   ami = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.generated_key.key_name
 
   user_data = file("/var/lib/jenkins/ssh_connection.sh")
 
