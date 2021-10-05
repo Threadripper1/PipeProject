a5c3d6ec1ae57577ae7cd65825e68efbd7550fa8
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-05 17:09:37 +0300

    Update terraform file

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -9,15 +9,13 @@ terraform {
   }
 }
 
-variable "key_WebServer" {}
-
 resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "generated_key" {
-  key_name   = var.key_WebServer
+  key_name   = "WebServer"
   public_key = tls_private_key.example.public_key_openssh
 }
 
