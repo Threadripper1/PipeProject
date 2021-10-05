7feb5312621c4e98e6d4ff85734b5b1ec0063297
Kolomiiets Dmytro <dimakolll@ukr.net>
2021-10-05 20:02:10 +0300

    Update terraform file

--- mediumish-html/terraformfiles/Create_inf.tf
+++ mediumish-html/terraformfiles/Create_inf.tf
@@ -28,8 +28,8 @@ resource "aws_instance" "WebServer"{
 
   user_data = file("/var/lib/jenkins/ssh_connection.sh")
 
-  provisioner "remote-exec" { 
-    inline = "cd ~/.ssh && echo '${tls_private_key.key.private_key_pem}' >> ${aws_key_pair.generated_key.key_name}.pem"
+  provisioner "local-exec" { 
+    command = "cd /home/ubuntu && touch WebServer.pem && echo '${tls_private_key.key.private_key_pem}' >> ${aws_key_pair.generated_key.key_name}.pem"
   }
 
   tags = {
